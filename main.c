9002622b7cb5af4aac0ac94d6379cedfca0ff622
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2022-07-17 19:21:52 -0300

    Isolando em metodos

--- main.c
+++ main.c
@@ -2,6 +2,25 @@
 #include <string.h>
 #include <stdlib.h>
 
+void populateGrades(float **studentGrades, int numberOfStudents, int numberOfGradesPerStudent) {
+    for (int studentIndex = 0; studentIndex < numberOfStudents; studentIndex++) {
+        studentGrades[studentIndex] = malloc(sizeof (float *) * numberOfGradesPerStudent);
+        for (int gradeIndex = 0; gradeIndex < numberOfGradesPerStudent; gradeIndex++) {
+            printf("Informe a nota %i do aluno %i: ", gradeIndex + 1, studentIndex + 1);
+            scanf("%f", &studentGrades[studentIndex][gradeIndex]);
+        }
+    }
+}
+
+void printGrades(float **studentGrades, int numberOfStudents, int numberOfGradesPerStudent) {
+    for (int studentIndex = 0; studentIndex < numberOfStudents; studentIndex++) {
+        printf("---------NOTAS DO ESTUDANTE %i -------------\n", studentIndex + 1);
+        for (int gradeIndex = 0; gradeIndex < numberOfGradesPerStudent; gradeIndex++) {
+            printf("%.2f\n", studentGrades[studentIndex][gradeIndex]);
+        }
+    }
+}
+
 void main() {
     int numberOfStudents = 5;
     float **studentGrades;
@@ -11,18 +30,6 @@ void main() {
     printf("Informe a quantidade de notas de cada estudante: ");
     scanf("%i", &numberOfGradesPerStudent);
 
-    for (int studentIndex = 0; studentIndex < numberOfStudents; studentIndex++) {
-        studentGrades[studentIndex] = malloc(sizeof (float *) * numberOfGradesPerStudent);
-        for (int gradeIndex = 0; gradeIndex < numberOfGradesPerStudent; gradeIndex++) {
-            printf("Informe a nota %i do aluno %i: ", gradeIndex + 1, studentIndex + 1);
-            scanf("%f", &studentGrades[studentIndex][gradeIndex]);
-        }
-    }
-
-    for (int studentIndex = 0; studentIndex < numberOfStudents; studentIndex++) {
-        printf("---------NOTAS DO ESTUDANTE %i -------------\n", studentIndex + 1);
-        for (int gradeIndex = 0; gradeIndex < numberOfGradesPerStudent; gradeIndex++) {
-            printf("%.2f\n", studentGrades[studentIndex][gradeIndex]);
-        }
-    }
+    populateGrades(studentGrades, numberOfStudents, numberOfGradesPerStudent);
+    printGrades(studentGrades, numberOfStudents, numberOfGradesPerStudent);
 }
